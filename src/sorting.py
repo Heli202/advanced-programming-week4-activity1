cb4de37755f25ce0bfaa00a3c8eccf7ff747b1f0
Heli202 <158405650+Heli202@users.noreply.github.com>
2025-08-14 15:12:44 +0800

    Created sorting.py with mutable and immutable sorting

--- src/sorting.py
+++ src/sorting.py
@@ -1,1 +1,12 @@
-pass
+from src.galaxy import Galaxy
+
+def my_mutating_sort(items: list):
+    return items.sort()
+
+def my_immutable_sort(items: list):
+    new_sorted_list = items
+    new_sorted_list.sort()
+    return new_sorted_list
+
+def galaxy_sorting_method(galaxies: list):
+    return my_mutating_sort(galaxies)
