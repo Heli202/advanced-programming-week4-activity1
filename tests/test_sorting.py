ceac075a62c603811e8c8ae879c1c9c1d978e19d
Heli202 <158405650+Heli202@users.noreply.github.com>
2025-08-14 15:56:13 +0800

    started unit tests - unfinished

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -3,26 +3,19 @@ from src.galaxy import Galaxy
 from src import sorting
 
 
-
 class TestSorting(unittest.TestCase):
     galaxies = []
+
     def setUp(self):
         for _ in range(20):
-            galaxies.append(Galaxy(Galaxy.random_galaxies_name_generator()))
-
-
-
-
+            self.galaxies.append(Galaxy(Galaxy.random_galaxies_name_generator()))
 
     def tearDown(self):
-        del galaxies
-        pass
+        del self.galaxies
 
-
-
+    def test_sorting(self):
+        self.galaxies = sorting.my_mutating_sort(self.galaxies) #unfinished
 
 
 if __name__ == '__main__':
-
     unittest.main()
-    print(TestSorting.setUp())
