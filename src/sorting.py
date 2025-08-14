2385cc1fbb86c55df5533a22ed5af253feb6c1d5
Heli202 <158405650+Heli202@users.noreply.github.com>
2025-08-14 15:15:04 +0800

    Fixed immutable sort
    Originally it would point to the same object in memory, but now it creates a new one so that it is truly immutable.

--- src/sorting.py
+++ src/sorting.py
@@ -4,7 +4,7 @@ def my_mutating_sort(items: list):
     return items.sort()
 
 def my_immutable_sort(items: list):
-    new_sorted_list = items
+    new_sorted_list = list(items)
     new_sorted_list.sort()
     return new_sorted_list
 
