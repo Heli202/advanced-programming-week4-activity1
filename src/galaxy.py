f0c7d77ca0127733897a1a12a6ef9c7b01a51e3b
GebwellB <170325747+GebwellB@users.noreply.github.com>
2025-08-14 15:17:29 +0800

    Updated dunders in galaxy.py

--- src/galaxy.py
+++ src/galaxy.py
@@ -4,8 +4,11 @@ class Galaxy:
     def __init__(self, galaxy_name: str):
         self.galaxy_name = galaxy_name
 
-    def __str__(self):
+    def __repr__(self):
         return f'Galaxy Name: {self.galaxy_name}'
+
+    def __str__(self):
+        return self.galaxy_name
 
     def __hash__(self):
         return hash(self.galaxy_name)
@@ -31,21 +34,21 @@ class Galaxy:
             return self.galaxy_name > other
         return NotImplemented
 
-    def __le__(self, other):
+    def __le__(self, other: object) -> bool:
         if isinstance(other, Galaxy):
             return self.galaxy_name <= other.galaxy_name
         if isinstance(other, int):
             return self.galaxy_name <= other
         return NotImplemented
 
-    def __ge__(self, other):
+    def __ge__(self, other: object) -> bool:
         if isinstance(other, Galaxy):
             return self.galaxy_name >= other.galaxy_name
         if isinstance(other, int):
             return self.galaxy_name >= other
         return NotImplemented
 
-    def __ne__(self, other):
+    def __ne__(self, other: object) -> bool:
         if isinstance(other, Galaxy):
             return self.galaxy_name != other.galaxy_name
         if isinstance(other, int):
@@ -77,4 +80,4 @@ if __name__ == '__main__':
         "NGC 4414"
     ]
 
-    print(Galaxy(galaxies).__ne__(Galaxy(galaxies)))
+    print(Galaxy(galaxies[1]))
